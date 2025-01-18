00b5e536f5b214cbbae2116ac1185769d0981886
Ubuntu <azureuser@aaa.1dtdcwon0jae5iaqijigfredde.bx.internal.cloudapp.net>
2025-01-18 21:17:33 +0000

    aaa

--- j.bat
+++ j.bat
@@ -1,4 +1,4 @@
-rem Compile all java files and run the class specified as parameter 1
+rem Compile all java files and run %1
 set j=C:\Users\Phill\java\jdk-23.0.1\bin
 %j%\javac -d  Classes *.java
 %j%\java  -cp Classes  com.AppaApps.Silicon.%1
