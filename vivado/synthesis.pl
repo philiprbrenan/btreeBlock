363a7823d78f89357471d48f9dd6229ede0fb80d
Philip R Brenan <philiprbrenan@gmail.com>
2025-02-03 19:36:43 +0000

    aaa

--- vivado/synthesis.pl
+++ vivado/synthesis.pl
@@ -4,10 +4,14 @@ use warnings FATAL => qw(all);
 use strict;
 use Carp;
 
-my $home    =  $ENV{HOME};                                                      # Home folder
-my $project = "$home/btreeBlock/vivado/";                                           # Project we wish to compile
-my $vivado  = "$home/Vivado/2024.2/";                                               # Location of vivaldo
+my $home      =  $ENV{HOME};                                                    # Home folder
+my $project   = "$home/btreeBlock/vivado/";                                     # Project we wish to compile
+my $synthesis = "$project/synthesis.tcl";                                       # Location of vivaldo
+my $vivado    = "$home/Vivado/2024.2/";                                         # Location of vivaldo
+my $vivadoX   = "$home/Vivado/2024.2/bin/vivado";                               # Location of vivaldo executable
 
-die "No such path: $vivado" if ! -d $vivado;
+die "No such file: $synthesis" unless -f $synthesis;
+die "No such path: $vivado"    unless -d $vivado;
+die "No such file: $vivadoX"   unless -f $vivadoX;
 
-say STDERR qx($vivado/bin/vivado -mode batch -source $project/synthesize.tcl);
+say STDERR qx($vivadoX -mode batch -source $synthesis);
