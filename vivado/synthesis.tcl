574fbd6aeea138876f9d226ce0de6e0ec8fd0bf9
Philip R Brenan <philiprbrenan@gmail.com>
2025-02-03 21:25:24 +0000

    aaa

--- vivado/synthesis.tcl
+++ vivado/synthesis.tcl
@@ -1,15 +1,17 @@
 # Set the project directory
+set project find
+set key     2
 set home $env(HOME)
-set project_dir "${home}/btreeBlock/verilog/find/2"
+set project_dir "${home}/btreeBlock/verilog/${project}/${key}"
 
 # Set the path to the includes directory
 set includes_dir "${project_dir}/includes"
 
 # Create a new Vivado project
-create_project find ${project_dir}/vivado -part xc7z020clg484-1 -force
+create_project ${project} ${project_dir}/vivado -part xc7z020clg484-1 -force
 
 # Add the main Verilog file and the include files
-add_files ${project_dir}/find.v
+add_files ${project_dir}/${project}.v
 add_files -norecurse ${includes_dir}/M.vh
 add_files -norecurse ${includes_dir}/T.vh
 
@@ -23,11 +25,11 @@ launch_runs synth_1
 wait_on_run synth_1
 
 # Check the synthesis results
-if { [get_runs -completed] == 0 } {
-    puts "Synthesis failed."
-} else {
-    puts "Synthesis completed successfully."
-}
+#if { [get_runs -completed] == 0 } {
+#    puts "Synthesis failed."
+#} else {
+#    puts "Synthesis completed successfully."
+#}
 
 # Close the project
 close_project
