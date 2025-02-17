82bc9dbd9e99de8904153e57264c36f3d8dd2073
Philip R Brenan <philiprbrenan@gmail.com>
2025-02-17 16:34:52 +0000

    aaa

--- pushToGitHub.pl
+++ pushToGitHub.pl
@@ -22,11 +22,11 @@ my @ext  = qw(.java .md .pl .txt .png .py .sv .tb .v .xdc);                     
 sub writeGitIgnore(@)                                                           # Write ignore to select just the files we want
  {my (@files) = @_;
   my @g = ("*", map {"!$_"} @files);
-  owf(fpe($home, qw(.gitignore)), join "\\n", @files);
+  owf(fpe($home, qw(.gitignore)), join "\n", @g);
  }
 
 push my @files, searchDirectoryTreesForMatchingFiles($home, @ext);              # Files to upload
-        @files = grep {!m(\A\.|backups/|Classes/|verilog|vivado/runs/)} @files; # Remove files that do not need to be saved
+        @files = grep {!m(/\.|backups/|Classes/|verilog|vivado/runs/)} @files;  # Remove files that do not need to be saved
 my @java = map {fn $_}  grep {fe($_) eq q(java) && fn($_) !~ m(Able\Z)} @files; # Java files to test do not include interfaces
 
 if (0)                                                                          # Upload via github crud
