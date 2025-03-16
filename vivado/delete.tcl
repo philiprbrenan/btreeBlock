f4c5d2079ecbd863aa647a31836be3c060c5fec8
Ubuntu <azureuser@vivado.1r03pp2oww1ujhxiyp205wwrjc.bx.internal.cloudapp.net>
2025-03-16 22:39:43 +0000

    aaa

--- vivado/delete.tcl
+++ vivado/delete.tcl
@@ -1,29 +1,30 @@
-set_param general.maxThreads 1
+read_verilog /home/azureuser/btreeBlock/verilog/delete/3/statement/10/delete.v
+read_xdc     /home/azureuser/btreeBlock/vivado/constraints/xc7a50tcpg236.xdc
 
-read_verilog /home/phil/btreeBlock/verilog/delete/2//delete.v
-read_xdc     /home/phil/btreeBlock/vivado/constraints/xc7a200tffv1156-2.xdc
+synth_design -name delete -top delete -part xc7a50tcpg236 -include_dirs /home/azureuser/btreeBlock/verilog/delete/3/statement/10/includes/ -flatten_hierarchy none -no_timing_driven -directive AlternateRoutability
 
-synth_design -name delete -top delete -part xc7a200tffv1156-2 -include_dirs /home/phil/btreeBlock/verilog/delete/2/includes/ -flatten_hierarchy none
-write_checkpoint -force /home/phil/btreeBlock/verilog/delete/vivado/dcp//synth.dcp
+write_checkpoint -force /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/dcp/synth.dcp
 
-opt_design
-write_checkpoint -force /home/phil/btreeBlock/verilog/delete/vivado/dcp//opt.dcp
+opt_design -directive RuntimeOptimized
+write_checkpoint -force /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/dcp/opt.dcp
 
-report_utilization       -file /home/phil/btreeBlock/verilog/delete/vivado/reports//utilization.rpt
-report_methodology       -file /home/phil/btreeBlock/verilog/delete/vivado/reports//methodology.rpt
-report_timing_summary    -file /home/phil/btreeBlock/verilog/delete/vivado/reports//timing.rpt
-report_power             -file /home/phil/btreeBlock/verilog/delete/vivado/reports//power.rpt
-report_drc               -file /home/phil/btreeBlock/verilog/delete/vivado/reports//drc.rpt
-report_clock_interaction -file /home/phil/btreeBlock/verilog/delete/vivado/reports//clock_interaction.rpt
-report_cdc               -file /home/phil/btreeBlock/verilog/delete/vivado/reports//cdc.rpt
-report_control_sets      -file /home/phil/btreeBlock/verilog/delete/vivado/reports//control.rpt
-report_bus_skew          -file /home/phil/btreeBlock/verilog/delete/vivado/reports//bus_skew.rpt
-report_high_fanout_nets  -file /home/phil/btreeBlock/verilog/delete/vivado/reports//fanout.rpt
+report_bus_skew -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/bus_skew.rpt
+report_clock_interaction -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/clock_interaction.rpt
+report_control_sets -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/control_sets.rpt
+report_cdc -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/cdc.rpt
+report_design_analysis -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/design_analysis.rpt
+report_drc -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/drc.rpt
+report_high_fanout_nets -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/high_fanout_nets.rpt
+report_methodology -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/methodology.rpt
+report_power -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/power.rpt
+report_timing_summary -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/statement/10/timing_summary.rpt
+report_utilization -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/utilization.rpt
 
-place_design
-write_checkpoint -force /home/phil/btreeBlock/verilog/delete/vivado/dcp//place.dcp
+place_design -directive AltSpreadLogic_high
+write_checkpoint -force /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/dcp/place.dcp
 
-route_design
-write_checkpoint -force /home/phil/btreeBlock/verilog/delete/vivado/dcp//route.dcp
+route_design -directive Quick
+write_checkpoint -force /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/dcp/route.dcp
+report_timing_summary    -file /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/reports/timing_route.rpt
 
-write_bitstream  -force /home/phil/btreeBlock/verilog/delete/2//final.bit
+write_bitstream  -force /home/azureuser/btreeBlock/verilog/delete/3/statement/10/vivado/delete.bit
